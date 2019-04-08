935b3a5c53b6d93bb45034e68b8e19b7317f4e93
Alison Golden <alisong779@yahoo.com>
2019-04-08 03:29:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
- batch_badge_creator(attendees).each do print assign_rooms(attendees)
+ attendees.each do print batch_badge_creator(attendees) print assign_rooms(attendees)
 end
 end 
