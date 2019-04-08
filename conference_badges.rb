0e86fdb3319573a3483cad6a1468d74685ca1e6e
Alison Golden <alisong779@yahoo.com>
2019-04-08 02:42:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,9 +11,17 @@ array
 end
 
 def assign_rooms(attendees)
-  rooms = [1..7]
   room_assignments = []
-  attendees.each do |speaker| y = puts "Hello, #{speaker}! You'll be assinged to room #{rooms}!"
-  room_assignments.push(y)
+  attendees.each_with_index {|x, rooms| room_assignments<< "Hello, #{x}! You'll be assigned to room #{rooms+1}!"}
+  room_assignments
+end
+
+def printer(attendees)
+  arr = []
+  attendees.each_with_index do |a| batch_badge_creator(attendees) assign_rooms(attendees)
+  arr << a
+
+  return arr
 end
 end
+
